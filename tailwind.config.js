ba0c8dee6172615cd75d87e156a06921ef22b76b
Vignesh Prakasam <vigneshp.ceg@gmail.com>
2024-10-02 18:19:16 +0530

    applied style guide colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        limeGreen: 'hsl(75, 94%, 57%)',
+        grey700: 'hsl(0, 0%, 20%)',
+        grey800: 'hsl(0, 0%, 12%)',
+        grey900: 'hsl(0, 0%, 8%)',
+      },
       fontFamily: {
       custom: ['Inter', 'sans-serif'], 
       },
